0978bf7dcc8a8052e476631f2ebc5b6fcdfec54c
RonakWithCode <r7023j@gmail.com>
2024-03-23 23:46:03 -0700

    add filter

--- project/src/Components/News.jsx
+++ project/src/Components/News.jsx
@@ -1,49 +1,91 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
 
 export class News extends Component {
-  articles = []
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      articles: this.articles
-    }
-
-  
-
-
+      articles: [],
+      page: 1,
+      pageSize: 9, // Number of articles per page
+      totalResults: 0,
+      category: '',
+      country: 'us', // Default country
+      filters: ['business', 'entertainment', 'health', 'science', 'sports', 'technology'], // Available categories
+    };
   }
-  
+
   async componentDidMount() {
-    const API_KEY = import.meta.env.VITE_API;
-    const API = "https://newsapi.org/v2/top-headlines?country=us&apiKey="+API_KEY
-    const data = await fetch(API);
-    const dataJson = await data.json()
-    // console.log(dataJson);
-    this.setState({articles:dataJson.articles})
+    this.fetchArticles();
   }
 
+  async fetchArticles() {
+    const { page, pageSize, category, country } = this.state;
+    const API_KEY = import.meta.env.VITE_API;
+    const API = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=${pageSize}&page=${page}&apiKey=${API_KEY}`;
 
+    const data = await fetch(API);
+    const dataJson = await data.json();
+    this.setState({
+      articles: dataJson.articles,
+      totalResults: dataJson.totalResults,
+    });
+  }
 
+  handlePageChange = (newPage) => {
+    this.setState({ page: newPage }, () => {
+      this.fetchArticles();
+    });
+  };
+
+  handleCategoryChange = (category) => {
+    this.setState({ category, page: 1 }, () => {
+      this.fetchArticles();
+    });
+  };
 
   render() {
+    const { articles, page, pageSize, totalResults, filters, category } = this.state;
+    const totalPages = Math.ceil(totalResults / pageSize);
+
     return (
-      <div className='container  my-3'>
+      <div className="container my-3">
         <h1>Top news today</h1>
-        <div className="row">
-
-        {this.state.articles.map((elemnet,indix)=>{
-          return <div key={indix+elemnet.url} className="col-md-4">
-            <NewsItem  title={elemnet.title.slice(0,35)} dec={elemnet.description.slice(0,88)} NewsImage={elemnet.urlToImage} NewsUrl={elemnet.url} />
-           </div>
-        })}
+        {/* Filter and Category selection */}
+        <div className="mb-3">
+          <select onChange={(e) => this.handleCategoryChange(e.target.value)} value={category}>
+            <option value="">All Categories</option>
+            {filters.map((filter) => (
+              <option key={filter} value={filter}>
+                {filter.charAt(0).toUpperCase() + filter.slice(1)}
+              </option>
+            ))}
+          </select>
         </div>
-
-
-        
+        <div className="row">
+          {articles.map((element, index) => {
+            return (
+              <div key={index + element.url} className="col-md-4">
+                <NewsItem title={element.title} dec={element.description} NewsImage={element.urlToImage} NewsUrl={element.url} />
+              </div>
+            );
+          })}
+        </div>
+        {/* Pagination */}
+        <nav>
+          <ul className="pagination">
+            {Array.from({ length: totalPages }, (_, i) => (
+              <li key={i} className={`page-item ${i + 1 === page ? 'active' : ''}`}>
+                <button className="page-link" onClick={() => this.handlePageChange(i + 1)}>
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
